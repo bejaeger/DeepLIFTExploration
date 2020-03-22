631f3478f1c75f5bf292c795298ecb478202e2a9
Benjamin Paul Jaeger <benjamin.jaeger@cern.ch>
2020-03-21 20:07:34 -0700

    Update test script

--- test.py
+++ test.py
@@ -2,18 +2,16 @@ import ROOT
 import os
 
 
-
-f = ROOT.TFile.Open("atlas-open-data/mc_345323.VBFH125_WW2lep.exactly2lep.root")
-
+f = ROOT.TFile.Open("../atlas-open-data/mc_345323.VBFH125_WW2lep.exactly2lep.root")
 t = f.Get("mini")
 
 print("First 10 branch names:")
 for i, n in enumerate(t.GetListOfBranches()):
     print(n)
-    if i > 10: break
+    if i >= 10: break
 
 for i, e in enumerate(t):
-    print(e.jet_n)
-    if i > 10: break
+    print("Event = {}: nJets = {}".format(i, e.jet_n))
+    if i >= 10: break
     
 print("End!")
