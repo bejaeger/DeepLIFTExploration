05d055656b805cf92b4588350759296891c8382f
Benjamin Paul Jaeger <benjamin.jaeger@cern.ch>
2020-03-25 17:57:12 -0700

    Update setup script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "Calling setup.sh script"
 
 # setup script for a few shortcuts
 alias pullimg='docker pull gitlab-registry.cern.ch/bejaeger/deepliftforhep'
-alias runcont='docker run --rm -it -v $PWD:/home/docker/DeepLIFTforHEP gitlab-registry.cern.ch/bejaeger/deepliftforhep:latest /bin/bash'
+alias runcont='docker run --rm -it -v $PWD:/home/DeepLIFTforHEP gitlab-registry.cern.ch/bejaeger/deepliftforhep:latest /bin/bash'
 alias buildimglocal='docker build -f Dockerfile -t deepliftforhep:latest --rm=true --force-rm=true .'
 alias runcontlocal='docker run --rm -it -v $PWD:/home/docker/DeepLIFTforHEP deepliftforhep:latest /bin/bash'
 
